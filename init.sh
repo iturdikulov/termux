0bc8452a4e41deaed83e3eff625aff673c054012
Inom Turdikulov <i@inomoz.ru>
2024-11-21 13:19:04 +0300

    Initialize termux quickstart script

---
+++ init.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+echo "Welcome to Termux automatic installer"
+
+# Update & upgrade existing packages
+pkg update
+pkg upgrade -y
+
+# Configure storage
+termux-setup-storage
+
+# Install packages
+for each_pac in "wget" "net-tools" "mlocate"\
+                "python3" "clang" "zip" \
+                "unzip" "tree" "git" \
+                "cmatrix" "htop" "neovim" \
+                "golang" "nodejs" "python" \
+                "npm" "ruby" \
+                "ruby" "curl"; do
+    pkg install $each_pac -y
+done
+
+# Install Python packages
+for i in "setuptools" "requests" "ipython" "cryptography"; do
+    pip3 install $i
+done
+
+# Instal npm packages
+for i in "typescript" "ts-node"; do
+    npm install $i
+done
+
+# Install Go packages
+go install github.com/x-motemen/gore/cmd/gore@latest
+
+# Install Rust toolchain with packages
+pkg install tur-repo
+pkg update
+pkg install rustc-nightly
+for i in "evcxr_repl"
+   cargo install $i
+done
